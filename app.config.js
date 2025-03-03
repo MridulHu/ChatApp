45c7b4ffc05a75739e1ac20cfe48039b946561a6
MridulHu <mriduldas0812@gmail.com>
2025-03-03 22:55:14 +0530

    update gradle and few dependencies

--- app.config.js
+++ app.config.js
@@ -1,45 +1,46 @@
 import 'dotenv/config';
 
 export default {
-  "expo": {
-    "name": "ChatApp",
-    "slug": "ChatApp",
-    "newArchEnabled": true,
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "jsEngine" : "hermes",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "updates": {
-      "fallbackToCacheTimeout": 0
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
-    },
-    "android": {
-      "package": "com.anonymous.ChatApp",
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      }
-    },
-    "web": {
-      "favicon": "./assets/favicon.png"
+  expo: {
+    name: "ChatApp",
+    slug: "ChatApp",
+    newArchEnabled: true,
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    jsEngine: "hermes",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
     extra: {
+      eas: {
+        projectId: "d770afbf-8f02-42dc-9027-5c821c22d103",
+      },
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID
-    }
-  }
-}
+      appId: process.env.APP_ID,
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: "com.anonymous.ChatApp",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+  },
+};
